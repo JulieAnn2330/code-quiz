1155a0911619f02d5bc91182c6924613c9f90a87
Julie Schaub <julie.schaub@hotmail.com>
2020-06-28 17:10:35 -0500

    updated files

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+    var timeleft = 90;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000);
+
+
 const btn = document.querySelector('#btn');
 // handle click button
 btn.onclick = function () {
