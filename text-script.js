aaed0e2358eb1ecf2617bb1268af58a3f61f3593
Julie Schaub <julie.schaub@hotmail.com>
2020-06-30 07:52:44 -0500

    updated files

--- text-script.js
+++
@@ -1,9 +0,0 @@
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
- }
-
- Question.prototype.correctAnswer = function(choice) {
-   return choice === this.answer;
- }
