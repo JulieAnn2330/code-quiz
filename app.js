203beb1c97c75599ebe17b075ff7ac790513a879
Julie Schaub <julie.schaub@hotmail.com>
2020-07-04 13:21:59 -0500

    final files

--- app.js
+++ app.js
@@ -11,25 +11,25 @@ var downloadTimer = setInterval(function () {
 }, 1000);
 
 // Handling of questions
-function Quiz(questions) {
+function codeQuiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
 }
 
 // Pulling the questions
-Quiz.prototype.getQuestionIndex = function () {
+codeQuiz.prototype.getjsQuestionIndex = function () {
     return this.questions[this.questionIndex];
 }
 
 // When the quiz is over
-Quiz.prototype.isEnded = function () {
+codeQuiz.prototype.isEnded = function () {
     return this.questions.length === this.questionIndex;
 }
 
 // Scoring the guesses
-Quiz.prototype.guess = function (answer) {
-    if (this.getQuestionIndex().correctAnswer(answer)) {
+codeQuiz.prototype.guess = function (answer) {
+    if (this.getjsQuestionIndex().correctAnswer(answer)) {
         this.score++;
         alert("Correct");
     }
@@ -43,14 +43,14 @@ Quiz.prototype.guess = function (answer) {
     this.questionIndex++;
 }
 // Items needed for array
-function Question(text, choices, answer) {
+function jsQuestion(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
 
 // Answer is correct
-Question.prototype.correctAnswer = function (choice) {
+jsQuestion.prototype.correctAnswer = function (choice) {
     return choice === this.answer;
 }
 
@@ -63,10 +63,10 @@ function populate() {
     else {
         // show Question
         var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
+        element.innerHTML = quiz.getjsQuestionIndex().text;
 
         // show Choices
-        var choices = quiz.getQuestionIndex().choices;
+        var choices = quiz.getjsQuestionIndex().choices;
         for (var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
@@ -106,10 +106,10 @@ function myFunction() {
   }
 
 function showScores() {
-    var gameOverHtml = "<h1>Result</h1>";
-    gameOverHtml += "<h2 id = 'score'> Your score is " + quiz.score + " out of 10! Thank you for playing, " + name + "!</h2>";
+    var gameOver = "<h1>Result</h1>";
+    gameOver += "<h2 id = 'score'> Your score is " + quiz.score + " out of 10! Thank you for playing, " + name + "!</h2>";
     var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHtml;
+    element.innerHTML = gameOver;
     // var leaderboardHtml = "<h1>Wall of Fame</h1>";
     // leaderboardHtml += "<h2 id = 'score'>" + name + " " + quiz.score + "</h2>";
     // var element = document.getElementById("leaderboard");
@@ -117,23 +117,25 @@ function showScores() {
     clearInterval(downloadTimer);
     document.getElementById("countdowntimer").innerHTML = "<h1>Game Over</h1>";
       };
-
+      setTimeout(function(){
+        window.location.href = 'wall_of_fame.html';
+     }, 10000);
   
  // Questions
 var questions = [
-    new Question("1. What does javaScript provide to code?", ["a. Curb Appeal", "b. Functionality", "c. The Foundation", "d. None of the above"], "b. Functionality"),
-    new Question("2. What is a simple defintion of javaScript?", ["a. JavaScript gives web pages interactive elements that engage a user.", "b. JavaScript gives structure and style to web pages.", "c. JavaScript defines rules for the construction of a document.", "d. None of the Above"], "a. JavaScript gives web pages interactive elements that engage a user."),
-    new Question("3. What are commonly used in computer programs to organize data so that a related set of values can be easily sorted or searched?", ["a. Functions", "b. Objects", "c. Arrays", "d. None of the above"], "c. Arrays"),
-    new Question("4. The following is an example of what? for (var i=0, i < userlength, i++)", ["a. Function", "b. Array", "c. Object", "d. For Loop"], "d. For Loop"),
-    new Question("5. In the index of an array is 5, how many items are inside the array?", ["a. 7", "b. 4", "c. -1", "d. 6"], "d. 6"),
-    new Question("6. What is a variable?", ["a. A request made by a program or script that performs a predetermined function.", "b. A block of code designed to perform a particular task.", "c. A container for storing data values.", "d. None of the Above"], "c. A container for storing data values."),
-    new Question("7. What is a function call?", ["a. A request made by a program or script that performs a predetermined function.", "b. A special variable, which can hold more than one value at a time.", "c. A method.", "d. None of the Above"], "a. A request made by a program or script that performs a predetermined function."),
-    new Question("8. If you had the string var txt = (ABCDEFGHIJKLMNOPQRSTUVWXYZ), var sln would equal what?" , ["a. txt", "b. (ABCDEFGHIJKLMNOPQRSTUVWXYZ)", "c. txt.length", "d. 26"], "c. txt.length"),
-    new Question("9. How do you call the function showScore??", ["a. showScore()", "b. Undefined", "c. function ()", "d. function showScore(score)"], "a. showScore()"),
-    new Question("10. Strict equality means values which we are comparing must have the same type, and is represented by which operator?", ["a. =", "b. ===", "c. ==", "d. >="], "b. ==="),
+    new jsQuestion("1. What does javaScript provide to code?", ["a. Curb Appeal", "b. Functionality", "c. The Foundation", "d. None of the above"], "b. Functionality"),
+    new jsQuestion("2. What is a simple defintion of javaScript?", ["a. JavaScript gives web pages interactive elements that engage a user.", "b. JavaScript gives structure and style to web pages.", "c. JavaScript defines rules for the construction of a document.", "d. None of the Above"], "a. JavaScript gives web pages interactive elements that engage a user."),
+    new jsQuestion("3. What are commonly used in computer programs to organize data so that a related set of values can be easily sorted or searched?", ["a. Functions", "b. Objects", "c. Arrays", "d. None of the above"], "c. Arrays"),
+    new jsQuestion("4. The following is an example of what? for (var i=0, i < userlength, i++)", ["a. Function", "b. Array", "c. Object", "d. For Loop"], "d. For Loop"),
+    new jsQuestion("5. In the index of an array is 5, how many items are inside the array?", ["a. 7", "b. 4", "c. -1", "d. 6"], "d. 6"),
+    new jsQuestion("6. What is a variable?", ["a. A request made by a program or script that performs a predetermined function.", "b. A block of code designed to perform a particular task.", "c. A container for storing data values.", "d. None of the Above"], "c. A container for storing data values."),
+    new jsQuestion("7. What is a function call?", ["a. A request made by a program or script that performs a predetermined function.", "b. A special variable, which can hold more than one value at a time.", "c. A method.", "d. None of the Above"], "a. A request made by a program or script that performs a predetermined function."),
+    new jsQuestion("8. If you had the string var txt = (ABCDEFGHIJKLMNOPQRSTUVWXYZ), var sln would equal what?" , ["a. txt", "b. (ABCDEFGHIJKLMNOPQRSTUVWXYZ)", "c. txt.length", "d. 26"], "c. txt.length"),
+    new jsQuestion("9. How do you call the function showScore??", ["a. showScore()", "b. Undefined", "c. function ()", "d. function showScore(score)"], "a. showScore()"),
+    new jsQuestion("10. Strict equality means values which we are comparing must have the same type, and is represented by which operator?", ["a. =", "b. ===", "c. ==", "d. >="], "b. ==="),
  ];
 
-var quiz = new Quiz(questions);
+var quiz = new codeQuiz(questions);
 
 populate();
 
--- wall_of_fame.js
+++ wall_of_fame.js
@@ -1,86 +1,42 @@
-var playerInput = document.querySelector("#player-text");
-var playerForm = document.querySelector("#player-form");
-var playerList = document.querySelector("#player-list");
-var scoreCountSpan = document.querySelector("#score-count");
-var players = ["name + quiz.score", "Learn CSS", "Learn JavaScript"];
-
-renderPlayers();
-function renderPlayers() {
-  // Clear playerList element and update playerCountSpan
-  playerList.innerHTML = "";
-  scoreCountSpan.textContent = players.length;
-
-  // Render a new li for each player
-  for (var i = 0; i < players.length; i++) {
-    var highScore = players[i];
-
-    var li = document.createElement("li");
-    li.textContent = highScore;
-    playerList.appendChild(li);
+function generate_table() {
+    // get the reference for the body
+    var body = document.getElementsByTagName("body")[0];
   
-    var button = document.createElement("button");
-    button.textContent = "Complete";
+    // creates a <table> element and a <tbody> 
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+   
+    var player = [];
 
-    li.appendChild(button);
-    playerList.appendChild(li);
+    if (startGame === true) {
+    Array.prototype.push.apply(player, name);
+     }
+  
+    // creating all cells
+    for (var i = 0; i < 10; i++) {
+      // creates a table row
+      var row = document.createElement("tr");
+  
+      for (var j = 0; j < 3; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+       var cell = document.createElement("td");
+        var cellText = document.createTextNode(player);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+  
+
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+  
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
+    tbl.setAttribute("align", "center")
   }
-}
-
-function init() {
-  // Get stored players from localStorage
-  // Parsing the JSON string to an object
-  var storedPlayers = JSON.parse(localStorage.getItem("scores"));
-
-  // If playerswere retrieved from localStorage, update the todos array to it
-  if (storedPlayers !== null) {
-    players = storedPlayers;
-  }
-
-  // Render players to the DOM
-  renderPlayers();
-}
-
-function storePlayers() {
-  // Stringify and set "todos" key in localStorage to todos array
-  localStorage.setItem("scores", JSON.stringify(players));
-}
-
-// When form is submitted...
-playerForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  var playerText = playerInput.value.trim();
-
-  // Return from function early if submitted todoText is blank
-  if (playerText === "") {
-    return;
-  }
-
-  // Add new todoText to todos array, clear the input
-  players.push(playerText);
-  playerInput.value = "";
-
-  // Store updated todos in localStorage, re-render the list
-  storePlayers();
-  renderPlayers();
-});
-
-// When a element inside of the todoList is clicked...
-// playerList.addEventListener("click", function(event) {
-//   var element = event.target;
-
-  // If that element is a button...
-  // if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
-    // var index = element.parentElement.getAttribute("data-index");
-    // players.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
-    storePlayers();
-    renderPlayers();
-  // }
-// });
-
-function myFunction() {
-  location.replace("./index.html")
-}
